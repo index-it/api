f41d4c7d9116c0afb19035a765acc12341878691
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-02-17 11:08:24 +0100

    feat: openapi init

--- src/main/kotlin/app/index_it/api/routing/Routing.kt
+++ src/main/kotlin/app/index_it/api/routing/Routing.kt
@@ -4,9 +4,12 @@ import app.index_it.api.routing.auth.auth
 import app.index_it.core.db.NotifyDBM
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.swagger.codegen.v3.generators.html.StaticHtmlCodegen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.litote.kmongo.id.serialization.IdKotlinXSerializationModule
@@ -19,6 +22,14 @@ fun Application.configureRouting() {
     }
 
     routing {
+        /**
+         * Unsure if this is good for the documentation or not
+        swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
+        openAPI(path="openapi", swaggerFile = "openapi/documentation.yaml") {
+            codegen = StaticHtmlCodegen()
+        }
+        **/
+
         // TODO: Remove
         get("/notify/{email}") {
             val email = withContext(Dispatchers.IO) {
