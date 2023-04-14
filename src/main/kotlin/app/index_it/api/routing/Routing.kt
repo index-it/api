ad5989914c715254c7cf05d8cba6ed31b3639a78
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-04-14 16:26:01 +0200

    feat: cors

--- src/main/kotlin/app/index_it/api/routing/Routing.kt
+++ src/main/kotlin/app/index_it/api/routing/Routing.kt
@@ -5,6 +5,7 @@ import app.index_it.api.routing.auth.authRoutes
 import app.index_it.api.routing.kube.kubeRoutes
 import app.index_it.api.routing.list.listRoutes
 import app.index_it.api.routing.user.userRoutes
+import app.index_it.api.routing.web.webRoutes
 import app.index_it.api.routing.websocket.websocketRoutes
 import io.ktor.server.application.*
 import io.ktor.server.resources.*
@@ -20,6 +21,7 @@ fun Application.configureRouting() {
     routing {
         kubeRoutes()
         adminRoutes()
+        webRoutes()
         authRoutes()
         userRoutes()
         listRoutes()
