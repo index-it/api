aec319201fb91741d9eb3602a0eb911d946096c3
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-06-23 15:41:26 +0200

    fix: serializable annotation

--- src/main/kotlin/app/index_it/models/templates/ListColorsDto.kt
+++ src/main/kotlin/app/index_it/models/templates/ListColorsDto.kt
@@ -2,10 +2,12 @@ package app.index_it.models.templates
 
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import org.bson.types.ObjectId
 import org.litote.kmongo.Id
 import org.litote.kmongo.id.toId
 
+@Serializable
 data class ListColorsDto(
     @Contextual @SerialName("_id") val id: Id<ListColorsDto> = ObjectId().toId(),
     val description: String,
