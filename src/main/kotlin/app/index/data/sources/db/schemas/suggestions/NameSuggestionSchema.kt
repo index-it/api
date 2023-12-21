b000f4435d84f06854da0c334bf58442600a4d92
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-12-22 00:40:45 +0100

    fix: [ci skip] id and locale index

--- src/main/kotlin/app/index/data/sources/db/schemas/suggestions/NameSuggestionSchema.kt
+++ src/main/kotlin/app/index/data/sources/db/schemas/suggestions/NameSuggestionSchema.kt
@@ -14,10 +14,10 @@ import org.jetbrains.exposed.sql.VarCharColumnType
  * @property locale
  */
 object NameSuggestionTable : Table() {
-    val id = integer("id").autoIncrement()
+    val id = integer("id").autoIncrement().index()
+    val locale = char("locale", 2).index()
     val description = varchar("description", 100)
     val names = array<String>("ix_names", VarCharColumnType(150))
-    val locale = char("locale", 2)
 
     override val primaryKey = PrimaryKey(id, locale)
 }
