07c0123c3e94b704bb3818f34fecda08870f5dd8
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2024-02-28 15:29:55 +0100

    fix: bad request exception

--- src/main/kotlin/app/index/api/plugins/StatusPages.kt
+++ src/main/kotlin/app/index/api/plugins/StatusPages.kt
@@ -4,6 +4,7 @@ import app.index.core.exceptions.AuthenticationException
 import app.index.core.exceptions.AuthorizationException
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
@@ -19,6 +20,9 @@ fun Application.configureStatusPages() {
         }
         exception<RequestValidationException> { call, cause ->
             call.respond(HttpStatusCode.BadRequest, cause.reasons.joinToString("\n"))
+        }
+        exception<BadRequestException> { call, _ ->
+            call.respond(HttpStatusCode.BadRequest)
         }
         exception<Exception> { call, cause ->
             call.application.environment.log.error(cause)
