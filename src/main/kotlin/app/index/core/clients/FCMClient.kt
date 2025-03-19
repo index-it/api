801c198bcb25af8015e718393440ddfcab72cd48
Giuliopime <giuliopime@gmail.com>
2025-03-19 14:13:41 +0100

    feat: add notifications for iOS

--- src/main/kotlin/app/index/core/clients/FCMClient.kt
+++ src/main/kotlin/app/index/core/clients/FCMClient.kt
@@ -6,9 +6,12 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.AndroidConfig
+import com.google.firebase.messaging.ApnsConfig
+import com.google.firebase.messaging.Aps
 import com.google.firebase.messaging.FcmOptions
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.MulticastMessage
+import com.google.firebase.messaging.Notification
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koin.core.annotation.Single
 
@@ -41,11 +44,13 @@ class FCMClient {
         val message =
             MulticastMessage.builder()
                 .addAllTokens(registrationToken)
+                .setNotification(Notification.builder().setTitle("Task reminder").setBody("Handle this in app").build())
                 .putData("type", "task-reminder")
                 .putData("task-id", taskId.toString())
                 .putData("task-name", taskName)
                 .setFcmOptions(FcmOptions.withAnalyticsLabel(taskReminderAnalyticsLabel))
                 .setAndroidConfig(AndroidConfig.builder().setPriority(AndroidConfig.Priority.HIGH).build())
+                .setApnsConfig(ApnsConfig.builder().putHeader("priority", "high").build())
                 .build()
 
         firebaseMessaging.sendEachForMulticast(message)
