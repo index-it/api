df37e790dd0b8ad39d690dae7fb61219f67a00c9
Giuliopime <giuliopime@gmail.com>
2026-05-28 22:20:45 +0200

    feat: cicd image name

--- services/api/build.gradle.kts
+++ services/api/build.gradle.kts
@@ -88,7 +88,7 @@ jib {
     }
 
     to {
-        image = "ghcr.io/${System.getenv("GHCR_ORGANIZATION")}/${project.name}"
+        image = "ghcr.io/${System.getenv("GHCR_ORGANIZATION")}/index_api"
         tags = setOf(System.getenv("COMMIT_SHA"), project.version.toString(), "latest")
         auth {
             username = System.getenv("GHCR_ACTOR")
